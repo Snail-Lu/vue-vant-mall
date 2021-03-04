a6dc937d35f8ed3037faaad2c45c49e4ff5c9211
Snail-Lu <snaillu@yeah.net>
2021-03-04 23:29:36 +0800

    feat: axios请求简单封装

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { get, post } from './utils/http';
 
 Vue.config.productionTip = false
+Vue.prototype.$http = { get, post }
 
 function initRem() {
   let cale = window.screen.availWidth > 750 ? 2 : window.screen.availWidth / 375
--- vue.config.js
+++ vue.config.js
@@ -34,45 +34,7 @@ module.exports = {
         },
       },
       // 启用 CSS modules for all css / pre-processor files.
-      modules: false,
+      requireModuleExtension: false,
     },
-  
-    // 是一个函数，允许对内部的 webpack 配置进行更细粒度的修改。
-    chainWebpack: (config) => {
-      // 配置别名
-      config.resolve.alias
-        .set('@', resolve('src'))
-        .set('assets', resolve('src/assets'))
-        .set('components', resolve('src/components'))
-        .set('views', resolve('src/views'))
-  
-      config.optimization.minimizer('terser').tap((args) => {
-        // 去除生产环境console
-        args[0].terserOptions.compress.drop_console = true
-        return args
-      })
-    },
-  
-    // 是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建。
-    parallel: require('os').cpus().length > 1,
-  
-    devServer: {
-      host: '0.0.0.0',
-      port: 8088, // 端口号
-      https: false, // https:{type:Boolean}
-      open: false, // 配置自动启动浏览器  open: 'Google Chrome'-默认启动谷歌
-  
-      // 配置多个代理
-      proxy: {
-        '/api': {
-          target: 'https://www.mock.com',
-          ws: true, // 代理的WebSockets
-          changeOrigin: true, // 允许websockets跨域
-          pathRewrite: {
-            '^/api': '',
-          },
-        },
-      },
-    },
-  }
+}
   
