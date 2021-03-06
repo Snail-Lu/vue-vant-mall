862b88d5533ec0fdef6816142d0b8f7f8e0973f5
Snail-Lu <snaillu@yeah.net>
2021-03-06 22:18:16 +0800

    feat: Home组件内容优化

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,15 @@ export default {
 </script>
 
 <style>
+html, body {
+  height: 100%;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f5f5f5;
+  min-height: 100%;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = {
         postcss: {
           plugins: [
             require('postcss-px2rem')({
-              remUnit: 75,
+              remUnit: 37.5,
             }),
           ],
         },
