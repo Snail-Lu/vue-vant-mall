8c2b4e009b6e3918ccdc46783b8a1651a65725ab
Snail-Lu <snaillu@yeah.net>
2021-03-06 13:54:25 +0800

    feat: 新增tabbar页包装组件

---
+++ src/components/Layout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="layout">
+    <slot></slot>
+    <footer>
+      <van-tabbar route>
+        <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+        <van-tabbar-item to="/category" icon="apps-o">分类</van-tabbar-item>
+        <van-tabbar-item to="/cart" icon="cart-o">购物车</van-tabbar-item>
+        <van-tabbar-item to="/my" icon="contact">我的</van-tabbar-item>
+      </van-tabbar>
+    </footer>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vant';
+export default {
+  name: 'Layout',
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
