1f6956ba449805488afbc7aa985d6da4321f09dc
Snail-Lu <snaillu@yeah.net>
2021-03-07 16:06:40 +0800

    feat: 分类页优化

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,20 +1,101 @@
 <template>
   <Layout>
     <div class="category">
-      <van-button type="primary">分类</van-button>
+      <van-tree-select
+        :items="categoryList"
+        :active-id.sync="activeId"
+        :main-active-index.sync="activeIndex"
+      />
     </div>
   </Layout>
 </template>
 
 <script>
-import { Button } from 'vant';
+import { Button, TreeSelect } from 'vant';
 import Layout from '../components/Layout';
 
 export default {
   name: 'Category',
   components: {
     [Button.name]: Button,
+    [TreeSelect.name]: TreeSelect,
     Layout
+  },
+  data: function() {
+    return {
+      categoryList: [
+        {
+          // 导航名称
+          text: '浙江',
+          // 该导航下所有的可选项
+          children: [
+            {
+              // 名称
+              text: '温州',
+              // id，作为匹配选中状态的标识符
+              id: 1,
+            },
+            {
+              text: '杭州',
+              id: 2,
+            },
+          ],
+        },
+        {
+          // 导航名称
+          text: '江苏',
+          // 该导航下所有的可选项
+          children: [
+            {
+              // 名称
+              text: '苏州',
+              // id，作为匹配选中状态的标识符
+              id: 3,
+            },
+            {
+              text: '南京',
+              id: 4,
+            },
+          ],
+        },
+        {
+          // 导航名称
+          text: '广东',
+          // 该导航下所有的可选项
+          children: [
+            {
+              // 名称
+              text: '深圳',
+              // id，作为匹配选中状态的标识符
+              id: 5,
+            },
+            {
+              text: '广州',
+              id: 6,
+            },
+          ],
+        },
+        {
+          // 导航名称
+          text: '福建',
+          // 该导航下所有的可选项
+          children: [
+            {
+              // 名称
+              text: '福州',
+              // id，作为匹配选中状态的标识符
+              id: 7,
+            },
+            {
+              text: '厦门',
+              id: 8,
+            },
+          ],
+        },
+      ],
+      activeId: 1,
+      activeIndex: 0,
+    }
   }
 }
 </script>
