22a1baacff4dec7effc1c96b4417d3006e9b118f
snaillu <snaillu@yeah.net>
2021-06-15 10:50:34 +0800

    feat: 路由配置修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,38 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
 // 使用vue-router
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    meta: { title: '首页', keepAlive: false }
-  },
-  {
-    path: '/category',
-    name: 'Category',
-    component: () => import('../views/Category.vue'),
-    meta: { title: '分类', keepAlive: false }
-  },
-  {
-    path: '/cart',
-    name: 'Cart',
-    component: () => import('../views/Cart.vue'),
-    meta: { title: '购物车', keepAlive: false }
-  },
-  {
-    path: '/my',
-    name: 'My',
-    component: () => import('../views/My.vue'),
-    meta: { title: '我的', keepAlive: false }
-  }
-];
-  
+	{
+		path: '/',
+		name: 'Home',
+		component: () => import('@views/pages/Home.vue'),
+		meta: { title: '首页', keepAlive: false }
+	},
+	{
+		path: '/category',
+		name: 'Category',
+		component: () => import('@views/pages/Category.vue'),
+		meta: { title: '分类', keepAlive: false }
+	},
+	{
+		path: '/cart',
+		name: 'Cart',
+		component: () => import('@views/pages/Cart.vue'),
+		meta: { title: '购物车', keepAlive: false }
+	},
+	{
+		path: '/my',
+		name: 'My',
+		component: () => import('@views/pages/My.vue'),
+		meta: { title: '我的', keepAlive: false }
+	}
+]
+
 const router = new VueRouter({
-  routes,
+	routes
 })
 
 export default router
