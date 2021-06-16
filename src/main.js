f69c7625bcca090595868b7158f883a4bfe20350
snaillu <snaillu@yeah.net>
2021-06-16 18:27:07 +0800

    feat: 全局样式引入

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { Get, Post } from './utils/request'
 import router from './router'
 import vant from './components/Vant/index'
+import '@/styles/index.scss' // global css
 
 // 批量注册vant组件
 Vue.use(vant)
