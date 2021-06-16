df078ed9db1320c7960248805eb005f747795081
snaillu <snaillu@yeah.net>
2021-06-16 18:37:14 +0800

    feat: 新增Vant文件

---
+++ src/components/Vant/index.js
@@ -0,0 +1,53 @@
+import {
+	Button,
+	Row,
+	Col,
+	Grid,
+	GridItem,
+	Image,
+	Search,
+	Swipe,
+	SwipeItem,
+	Card,
+	Tag,
+	List,
+	TreeSelect,
+	SubmitBar,
+	Checkbox,
+	Cell,
+	Lazyload,
+	NavBar,
+	Icon,
+	Dialog
+} from 'vant'
+
+let vantUIs = [
+	Button,
+	Row,
+	Col,
+	Grid,
+	GridItem,
+	Image,
+	Search,
+	Swipe,
+	SwipeItem,
+	Card,
+	Tag,
+	List,
+	TreeSelect,
+	SubmitBar,
+	Checkbox,
+	Cell,
+	Lazyload,
+	NavBar,
+	Icon,
+	Dialog
+]
+
+export default {
+	install(Vue) {
+		vantUIs.forEach((vantUI) => {
+			Vue.use(vantUI)
+		})
+	}
+}
