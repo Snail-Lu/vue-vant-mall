a24325cd68fed316fcedb101f084f0cd50a542c3
Snail-Lu <snaillu@yeah.net>
2021-07-26 23:32:26 +0800

    feat: 个人中心页部分图标改用iconfon图标

--- src/views/member/pages/My.vue
+++ src/views/member/pages/My.vue
@@ -9,11 +9,29 @@
 			</van-row>
 			<van-cell title="我的订单" is-link to="orderList" class="my-order" />
 			<van-grid :column-num="5">
-				<van-grid-item icon="photo-o" text="全部" />
-				<van-grid-item icon="photo-o" text="待付款" />
-				<van-grid-item icon="photo-o" text="待发货" />
-				<van-grid-item icon="photo-o" text="待收货" />
-				<van-grid-item icon="photo-o" text="退货/退款" />
+				<van-grid-item text="待付款">
+					<template #icon>
+						<i class="iconfont icon-pending"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="待发货">
+					<template #icon>
+						<i class="iconfont icon-bag"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="待收货">
+					<template #icon>
+						<i class="iconfont icon-track"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="已完成">
+					<template #icon>
+						<i class="iconfont icon-completed"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="退货/退款">
+					<template #icon> <i class="iconfont icon-money"></i> </template
+				></van-grid-item>
 			</van-grid>
 
 			<van-cell title="我的服务" is-link to="test" class="my-order" />
@@ -54,6 +72,10 @@ export default {
 
 	.my-order {
 		margin-top: 20px;
+	}
+
+	.iconfont {
+		font-size: 50px;
 	}
 }
 </style>
