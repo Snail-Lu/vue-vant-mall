f48a55db435c4ec1449b08bbb29c18c6d81b5b74
Snail-Lu <snaillu@yeah.net>
2021-07-27 23:28:02 +0800

    feat: 个人中心页样式优化

--- src/views/member/pages/My.vue
+++ src/views/member/pages/My.vue
@@ -1,12 +1,11 @@
 <template>
 	<Layout>
-		<div class="my">
-			<van-row class="header" type="flex" align="center">
-				<van-col span="6">
-					<van-image round width="60px" height="60px" src="https://img01.yzcdn.cn/vant/cat.jpeg" />
-				</van-col>
-				<van-col span="8">snail</van-col>
-			</van-row>
+		<div class="my-container">
+			<div class="header flex-box flex-v-center">
+				<van-image round width="60px" height="60px" src="https://img01.yzcdn.cn/vant/cat.jpeg" />
+				<div class="nick-name">不如山中做妖怪</div>
+				<div class="member-label">普通会员</div>
+			</div>
 			<van-cell title="我的订单" is-link to="orderList" class="my-order" />
 			<van-grid :column-num="5">
 				<van-grid-item text="待付款">
@@ -36,11 +35,31 @@
 
 			<van-cell title="我的服务" is-link to="test" class="my-order" />
 			<van-grid :column-num="5">
-				<van-grid-item icon="photo-o" text="优惠券" />
-				<van-grid-item icon="photo-o" text="红包" />
-				<van-grid-item icon="photo-o" text="地址簿" />
-				<van-grid-item icon="photo-o" text="收藏夹" />
-				<van-grid-item icon="photo-o" text="个人资料" />
+				<van-grid-item text="优惠券">
+					<template #icon>
+						<i class="iconfont icon-coupon"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="积分">
+					<template #icon>
+						<i class="iconfont icon-jifen"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="地址簿">
+					<template #icon>
+						<i class="iconfont icon-location"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="收藏夹">
+					<template #icon>
+						<i class="iconfont icon-star"></i>
+					</template>
+				</van-grid-item>
+				<van-grid-item text="个人资料">
+					<template #icon>
+						<i class="iconfont icon-gerenziliao"></i>
+					</template>
+				</van-grid-item>
 			</van-grid>
 		</div>
 	</Layout>
@@ -58,16 +77,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.my {
+.my-container {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	color: #2c3e50;
 
 	.header {
-		background-color: #fff;
-		padding: 20px;
+		background-image: linear-gradient(to bottom, #74b2f0, #45a0fc);
+		color: #fff;
+		height: 260px;
 		font-size: 20px;
+		padding: 0 40px;
+
+		.nick-name {
+			font-size: 36px;
+			margin-left: 20px;
+			max-width: 240px;
+			overflow: hidden;
+			text-overflow: ellipsis;
+			white-space: nowrap;
+		}
+
+		.member-label {
+			height: 30px;
+			font-size: 20px;
+			background-color: rgb(248, 190, 103);
+			line-height: 30px;
+			border-radius: 30px;
+			text-align: center;
+			padding: 0 10px;
+		}
 	}
 
 	.my-order {
