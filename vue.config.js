f0b9ad42a0afccff0b4adf2ccbc2f5bf7e33063c
Snial-Lu <snaillu@yeah.net>
2022-01-12 18:21:29 +0800

    代理配置修改

--- vue.config.js
+++ vue.config.js
@@ -60,5 +60,20 @@ module.exports = {
 				path.resolve(__dirname, './src/styles/mixin.scss')
 			]
 		}
+	},
+	devServer: {
+		proxy: {
+			// 匹配规则
+			'/api': {
+				// 要访问的跨域的域名
+				target: 'http://xxx.com/',
+				ws: true,
+				secure: false, // 使用的是http协议则设置为false，https协议则设置为true
+				changOrigin: true, //开启代理
+				pathRewrite: {
+					'^/api': '/api'
+				}
+			}
+		}
 	}
 }
